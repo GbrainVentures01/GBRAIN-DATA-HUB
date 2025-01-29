dff7bbd173ba71d590b224dec836df758da3ac2a
LayintonDev <layintondev@gmail.com>
2025-01-29 15:42:11 +0100

    9psb implementation

--- README.md
+++ README.md
@@ -2,3 +2,55 @@
    "start": "SET NODE_OPTIONS=--openssl-legacy-provider && react-scripts start",
    "build": "SET NODE_OPTIONS=--openssl-legacy-provider && react-scripts build"
  }, -->
+
+<!--
+           {user && user?.updateBvn && bankDetails?.length > 0 && user?.hasAccountNum && (
+                                                <div className={classes.account}>
+                                                    <Typography
+                                                        variant="body1"
+                                                        sx={{
+                                                            fontSize: '1.0rem',
+                                                            fontWeight: 500,
+                                                            mr: 1,
+                                                            mt: 1.75,
+                                                            mb: 0.75
+                                                        }}
+                                                    >
+                                                        Account Name
+                                                    </Typography>
+                                                    <div
+                                                        style={{
+                                                            display: 'flex',
+                                                            flexDirection: 'column',
+                                                            justifyContent: 'center',
+                                                            alignItems: 'center'
+                                                        }}
+                                                    >
+                                                        <div
+                                                            style={{
+                                                                display: 'flex',
+                                                                justifyContent: 'space-between',
+                                                                alignItems: 'center'
+                                                            }}
+                                                        >
+                                                            <Typography
+                                                                variant="body1"
+                                                                sx={{
+                                                                    fontSize: '1.0rem',
+                                                                    fontWeight: 500,
+                                                                    // mr: 1,
+                                                                    mt: 1.75,
+                                                                    mb: 0.75
+                                                                }}
+                                                                className={classes.acc_num}
+                                                            >
+                                                                {bankDetails[display]?.account_name?.slice(0, 20)}
+                                                            </Typography>
+                                                            {/* <CopyAll
+                                                                onClick={() => handleCopy({ d: bankDetails[display] })}
+                                                                sx={{ ml: 1 }}
+                                                            /> */}
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            )} -->
--- src/views/dashboard/Default/EarningCard.js
+++ src/views/dashboard/Default/EarningCard.js
@@ -335,56 +335,7 @@ const EarningCard = ({ isLoading, message }) => {
                                                     </div>
                                                 </div>
                                             )}
-                                            {user && user?.updateBvn && bankDetails?.length > 0 && user?.hasAccountNum && (
-                                                <div className={classes.account}>
-                                                    <Typography
-                                                        variant="body1"
-                                                        sx={{
-                                                            fontSize: '1.0rem',
-                                                            fontWeight: 500,
-                                                            mr: 1,
-                                                            mt: 1.75,
-                                                            mb: 0.75
-                                                        }}
-                                                    >
-                                                        Account Name
-                                                    </Typography>
-                                                    <div
-                                                        style={{
-                                                            display: 'flex',
-                                                            flexDirection: 'column',
-                                                            justifyContent: 'center',
-                                                            alignItems: 'center'
-                                                        }}
-                                                    >
-                                                        <div
-                                                            style={{
-                                                                display: 'flex',
-                                                                justifyContent: 'space-between',
-                                                                alignItems: 'center'
-                                                            }}
-                                                        >
-                                                            <Typography
-                                                                variant="body1"
-                                                                sx={{
-                                                                    fontSize: '1.0rem',
-                                                                    fontWeight: 500,
-                                                                    mr: 1,
-                                                                    mt: 1.75,
-                                                                    mb: 0.75
-                                                                }}
-                                                                className={classes.acc_num}
-                                                            >
-                                                                {bankDetails[display]?.account_name?.slice(0, 20)}
-                                                            </Typography>
-                                                            {/* <CopyAll
-                                                                onClick={() => handleCopy({ d: bankDetails[display] })}
-                                                                sx={{ ml: 1 }}
-                                                            /> */}
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            )}
+                                            {/* //Paste back her e */}
                                             {Cookies.get('user') ? (
                                                 <>
                                                     <Link
